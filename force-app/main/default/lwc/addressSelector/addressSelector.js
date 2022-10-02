7514db80f27ff4b7e5f56391ba42e4acd24c120d
Piotr PG Gajek <piotr.gajek95@gmail.com>
2022-10-02 20:23:48 +0200

    Init

---
+++ force-app/main/default/lwc/addressSelector/addressSelector.js
@@ -0,0 +1,53 @@
+import { LightningElement, wire, track } from 'lwc';
+
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
+
+import COUNTRY_CODE from '@salesforce/schema/Account.BillingCountryCode';
+import BILLING_STATE_CODE from '@salesforce/schema/Account.BillingStateCode';
+
+export default class AddressSelector extends LightningElement {
+    _countries = [];
+    _countryToStates = {};
+
+    selectedCountry;
+    selectedState;
+
+    @wire(getPicklistValues, {
+        recordTypeId: '012000000000000AAA',
+        fieldApiName: COUNTRY_CODE
+    })
+    wiredCountires({ data }) {
+        this._countries = data?.values;
+    }
+
+    @wire(getPicklistValues, { recordTypeId: '012000000000000AAA', fieldApiName: BILLING_STATE_CODE })
+    wiredStates({ data }) {
+        if (!data) {
+            return;
+        }
+
+        const validForToCountry = Object.fromEntries(Object.entries(data.controllerValues).map(([key, value]) => [value, key]));
+
+        this._countryToStates = data.values.reduce((accumulatedStates, state) => {
+            const countryIsoCode = validForToCountry[state.validFor[0]];
+
+            return { ...accumulatedStates, [countryIsoCode]: [...(accumulatedStates?.[countryIsoCode] || []), state] };
+        }, {});
+    }
+
+    get countries() {
+        return this._countries;
+    }
+
+    get states() {
+        return this._countryToStates[this.selectedCountry] || [];
+    }
+
+    handleCountry(e) {
+        this.selectedCountry = e.detail.value;
+    }
+
+    handleState(e) {
+        this.selectedState = e.detail.value;
+    }
+}
